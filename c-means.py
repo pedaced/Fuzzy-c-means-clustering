84b8f95bffbdd5d6eb9f71eb283addc7e3478904
= <=>
2019-05-03 22:42:07 +0430

    plot drawn and done!

--- c-means.py
+++ c-means.py
@@ -1,5 +1,9 @@
 import numpy as np
 import math
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
 
 def dist(p1, p2):
 	return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
@@ -9,6 +13,7 @@ def fcmeans(cluster_no, iteration_no, data):
 	clusters = []
 	n = len(data)
 	mu = np.zeros((n, cluster_no))
+	colors = ['b', 'r', 'g', 'c', 'm', 'y', 'k']
 
 	#Choosing random clusters
 	for i in range(cluster_no):
@@ -35,17 +40,30 @@ def fcmeans(cluster_no, iteration_no, data):
 
 	print(clusters)
 
-#test functions via random data set and cluster numer.
+	#drawing plot
+	data_set1 = []
+	data_set2 = []
+	for i in range(n):
+		data_set1.append(data[i][0])
+		data_set2.append(data[i][1])
 
-c = 2
-data_set = [[1, 3], [1.5, 3.2], [1.3, 2.6], [3, 1]]
-iteration = 10
-fcmeans(c, iteration, data_set)
-# cluster_no = 3
-# n = 200
-# iteration_no = 100
-# data_set = []
-# for i in range(n):
-# 	data_set.append([np.random.rand() * 10, np.random.rand() * 10])
-# data_set[0][0] = 0
-# print(data_set)
+	fig = plt.figure()
+	ax = fig.subplots()
+
+	for j in range(n):	
+		cluster = np.argmax(mu[j])
+		membership = mu[j][cluster]
+		plt.scatter(data_set1[j], data_set2[j], color = colors[cluster], alpha = membership)
+	plt.show()
+	plt.savefig("plot.png")	
+
+
+
+#test functions via random data set and cluster numer.
+cluster_no = 5
+n = 2000
+iteration_no = 100
+data_set = []
+for i in range(n):
+	data_set.append([np.random.rand() * 10, np.random.rand() * 10])
+fcmeans(cluster_no, iteration_no, data_set)
